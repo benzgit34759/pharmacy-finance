d28a4a27b34b9fc02b13fc70f7d5d796c0a59c04
benzgit34759 <pajetnipat@gmail.com>
2026-03-28 18:55:55 +0700

    fix: force dynamic rendering on all pages

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { redirect } from 'next/navigation'
 import { createClient } from '@/lib/supabase/server'
 import StaffForm from '@/components/StaffForm'
